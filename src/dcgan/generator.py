69baa42af5dc3cab14afc9aec272c9f5d07a2368
Pol Torres Alfonso <eichgammer@gmail.com>
2019-11-12 12:08:18 +0100

    more GAn models

--- src/dcgan/generator.py
+++ src/dcgan/generator.py
@@ -42,9 +42,9 @@ class Generator(nn.Module):
 
 
 # RasGAN uses image size of 256x256, kernel size = 4 x 4, stride=2, noise_size = 128
-class RasGenerator(nn.Module):
+class SevGenerator(nn.Module):
     def __init__(self, n_gpu=0, noise_vector_size=128, fm_depth=256, img_nc=3):
-        super(RasGenerator, self).__init__()
+        super(SevGenerator, self).__init__()
         self.ngpu = n_gpu
         self.noise_vector_size = noise_vector_size
         self.fm_depth = fm_depth
@@ -53,7 +53,7 @@ class RasGenerator(nn.Module):
         self.main = nn.Sequential(
             # input is Z, going into a convolution
             nn.ConvTranspose2d(self.noise_vector_size, self.fm_depth * 16, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(self.fm_depth * 8),
+            nn.BatchNorm2d(self.fm_depth * 16),
             nn.ReLU(True),
             # state size. 4096 x 4 x 4
             nn.ConvTranspose2d(self.fm_depth * 16, self.fm_depth * 8, 4, 2, 0, bias=False),
@@ -73,12 +73,50 @@ class RasGenerator(nn.Module):
             nn.ReLU(True),
             # state size. 256 x 64 x 64
             nn.ConvTranspose2d(self.fm_depth, int(self.fm_depth/2), 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.fm_depth),
+            nn.BatchNorm2d(int(self.fm_depth / 2)),
             nn.ReLU(True),
             # state size. 128 x 128 x 128
             nn.ConvTranspose2d(int(self.fm_depth / 2), img_nc, 4, 2, 1, bias=False),
             nn.Tanh())
             # state size. 3 x 256 x 256
+
+    def forward(self, input_data):
+        return self.main(input_data)
+
+
+class SixGenerator(nn.Module):
+    def __init__(self, n_gpu=0, noise_vector_size=100, fm_depth=128, img_nc=3):
+        super(SixGenerator, self).__init__()
+        self.ngpu = n_gpu
+        self.noise_vector_size = noise_vector_size
+        self.fm_depth = fm_depth
+        self.img_nc = img_nc
+
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(self.noise_vector_size, self.fm_depth * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(self.fm_depth * 16),
+            nn.ReLU(True),
+            # state size. 2048 x 4 x 4
+            nn.ConvTranspose2d(self.fm_depth * 16, self.fm_depth * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.fm_depth * 8),
+            nn.ReLU(True),
+            # state size. 1024 x 8 x 8
+            nn.ConvTranspose2d(self.fm_depth * 8, self.fm_depth * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.fm_depth * 4),
+            nn.ReLU(True),
+            # state size. 512 x 16 x 16
+            nn.ConvTranspose2d(self.fm_depth * 4, self.fm_depth * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.fm_depth * 2),
+            nn.ReLU(True),
+            # state size. 256 x 32 x 32
+            nn.ConvTranspose2d(self.fm_depth * 2, self.fm_depth, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.fm_depth),
+            nn.ReLU(True),
+            # state size. 128 x 64 x 64
+            nn.ConvTranspose2d(self.fm_depth, img_nc, 4, 2, 1, bias=False),
+            nn.Tanh())
+        # state size. 3 x 128 x 128
 
     def forward(self, input_data):
         return self.main(input_data)
